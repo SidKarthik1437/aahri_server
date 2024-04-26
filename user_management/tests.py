4cf3449be0123ddbd978779183e925fc97356e00
Sid Karthik <sa268592@gmail.com>
2024-04-27 01:12:52 +0530

    update tests

--- user_management/tests.py
+++ user_management/tests.py
@@ -6,11 +6,11 @@ from datetime import date
 
 class UserManagementTests(TestCase):
     def test_faculty_creation(self):
-        user = create_user('facultyuser', date(1980, 5, 15), 'faculty', employee_id='F001', position='Professor', password='testpass')
+        user = create_user('facultyuser', date(1980, 5, 15), 'faculty', faculty_usn='00GA00AD001', position='Professor', password='testpass')
         self.assertEqual(user.user.username, 'facultyuser')
         self.assertTrue(user.is_department_admin, False)
 
     def test_student_creation(self):
-        user = create_user('studentuser', date(2000, 12, 1), 'student', enrollment_number='S001')
+        user = create_user('studentuser', date(2000, 12, 1), 'student', usn='1GA20AD044')
         self.assertEqual(user.user.username, 'studentuser')
         self.assertEqual(user.enrollment_number, 'S001')
