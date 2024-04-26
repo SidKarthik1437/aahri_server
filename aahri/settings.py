88778fe9278fa5960bba9799a94f2dca70ea4056
Sid Karthik <sa268592@gmail.com>
2024-04-27 00:31:16 +0530

    added req.txt

--- aahri/settings.py
+++ aahri/settings.py
@@ -82,23 +82,23 @@ WSGI_APPLICATION = 'aahri.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ahri',
-#         'USER': 'admin',
-#         'PASSWORD': 'admin',
-#         'HOST': 'db',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ahri',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'db',
+        'PORT': '',
+    }
+}
 
 
 
